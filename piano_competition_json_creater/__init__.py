f288f91c6459143c9dfab5dc34f2f40173336158
artur.semikov <AG6CBZZ@mmm.com>
2024-08-11 14:55:28 +0300

    [8/11/2024]: Added examples of data files support, in `create_json.py`

---
+++ piano_competition_json_creater/__init__.py
@@ -0,0 +1,1 @@
+from .create_json import read_internal_input
--- piano_competition_json_creater/create_json.py
+++ piano_competition_json_creater/create_json.py
@@ -1,36 +1,54 @@
 import json as j
+import os
+from os import path
 import time
 
-brake_counter = 0
-jsons_array = []
 
-with open("input.txt", "r", encoding="utf-8") as file:
-    for line in file.read().splitlines():
+def read_internal_input():
+    print(f"Current working directory: {os.getcwd()}")
 
-        if brake_counter == 0:
-            json_obj = {"videoUrl": "", "composition": "", "name": str(line), "id": {
-                "$oid": f"{time.time()}"
-            }, "group": 'Номінація "Фортепіано соло"'}
+    executable_dir = path.dirname(__file__)
 
-            brake_counter += 1
-            continue
+    print(f"Current file name {__file__}")
 
-        if brake_counter == 1:
-            json_obj["composition"] = str(line)
-            brake_counter += 1
-            continue
+    print(f"Executable directory: {executable_dir}")
 
-        elif brake_counter == 2:
-            json_obj["videoUrl"] = str(line)
-            brake_counter += 1
-            continue
+    brake_counter = 0
 
-        elif brake_counter == 3:
-            jsons_array.append(json_obj)
-            brake_counter = 0
-            continue
+    jsons_array = []
 
-print(jsons_array)
+    with open(f"{executable_dir}/pianists_input.txt", "r", encoding="utf-8") as file:
+        for line in file.read().splitlines():
 
-with open('output.txt', 'w', encoding='utf8') as json_file:
-    j.dump(jsons_array, json_file, ensure_ascii=False, indent=3)
+            if brake_counter == 0:
+                json_obj = {"videoUrl": "", "composition": "", "name": str(line), "id": {
+                    "$oid": f"{time.time()}"
+                }, "group": 'Номінація "Фортепіано соло"'}
+
+                brake_counter += 1
+                continue
+
+            if brake_counter == 1:
+                json_obj["composition"] = str(line)
+                brake_counter += 1
+                continue
+
+            elif brake_counter == 2:
+                json_obj["videoUrl"] = str(line)
+                brake_counter += 1
+                continue
+
+            elif brake_counter == 3:
+                jsons_array.append(json_obj)
+                brake_counter = 0
+                continue
+
+    for e in jsons_array:
+        print(e)
+
+    # with open('output.txt', 'w', encoding='utf8') as json_file:
+    #    j.dump(jsons_array, json_file, ensure_ascii=False, indent=3)
+
+
+if __name__ == "__main__":
+    read_internal_input()
